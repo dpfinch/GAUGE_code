514efc45dca84fe034e9ae56a26e5c2cadb48e13
dpfinch <d.finch@ed.ac.uk>
2017-06-22 10:47:06 +0100

    Added a few extra bits including a tester to see if file actually exists

--- station_comparison.py
+++ station_comparison.py
@@ -12,9 +12,11 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import geos_info
 import brewer2mpl
+import sys
+import os.path
 ##==============================================================================
 
-def open_NOAA_flask_data(station):
+def NOAA_flask_data(station):
     if station.lower() == 'macehead':
         station = 'MHD'
     elif station.lower() == 'tacolnestion':
@@ -22,15 +24,22 @@ def open_NOAA_flask_data(station):
 
     if station != 'TAC' and station != 'MHD':
         print "No data for %s" % station
+        sys.exit()
 
     dirc = '/home/dfinch/Documents/CH4/NOAA_station_data/'
 
     fi = '%s%s_DISCRETE_CH4.dat' % (dirc, station)
 
+    if os.path.isfile(fi):
+        print "Opening file: %s" % fi
+    else:
+        print "No file named: %s" % fi
+        sys.exit()
+
     # This uses the assumption that the NOAA station data files have
     # comments at the beginning of the file commented with a '#'
     # This seems to hold true for all there station data files
-    all_raw_data = np.loadtxt(fi,dtype=str, comment = '#')
+    all_raw_data = np.loadtxt(fi,dtype=str, comments = '#')
 
     sample_dates = []
     ch4_value = []
@@ -45,9 +54,11 @@ def open_NOAA_flask_data(station):
 
         ch4_value.append(float(all_raw_data[x,11]))
 
-        return sample_dates, ch4_value
+    return sample_dates, ch4_value
 
+def simple_plot(dates,data):
 
+    pass
 
 ## =============================================================================
 ## END OF PROGRAM
